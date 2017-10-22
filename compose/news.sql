e41c8c8ef1decfa68e5a8ae9f770106f4947ac0b
Mr D <dqunbp@gmail.com>
2017-10-22 10:08:24 +0300

    refactor

---
+++ compose/news.sql
@@ -0,0 +1,8 @@
+INSERT INTO bot_news VALUES
+    ('123', 'https://life.ru/1053991', 
+    '{"title":"Трамп-младший "поместил" на обложку Time своего отца в костюме супергероя", "img":"https://vk.com/photo5010085_456239739"}',
+    now(), 12345);
+INSERT INTO bot_news VALUES
+    ('124', 'https://life.ru/1053985', 
+    '{"title":"Британец вышел из комы, услышав гимн любимой футбольной команды", "img":"https://vk.com/photo5010085_456239740"}',
+    now(), 12345);
